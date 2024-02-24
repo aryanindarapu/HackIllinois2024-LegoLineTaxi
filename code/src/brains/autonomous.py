f6d6a2bf0d09cc118c9e29a37250b6b48c654f9c
Seby <sebwyszynski@yahoo.com>
2024-02-24 16:33:46 -0600

    Blue Line Works!

--- code/src/brains/autonomous.py
+++ code/src/brains/autonomous.py
@@ -24,43 +24,44 @@ class Brain(base.Brain):
         image = cv2.rotate(self.camera.image_array, cv2.ROTATE_180)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSL)
-        cv2.imwrite('hsv_image.jpg', hsv_image)
+        hsl_image = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+        cv2.imwrite('hsl_image.jpg', hsl_image)
 
-        lower_yellow = np.array([20, 100, 100])
-        upper_yellow = np.array([30, 255, 255])
+        lower_blue = np.array([90, 70, 170])
+        upper_blue = np.array([150, 140, 255])
 
         # Threshold the HSV image to get only blue colors
-        mask = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
+        mask = cv2.inRange(hsl_image, lower_blue, upper_blue)
         cv2.imwrite('after_mask.jpg', mask)
 
         # Find contours in the mask
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        # if contours:
-        #     # Assume the largest contour is the blue line
-        #     largest_contour = max(contours, key=cv2.contourArea)
-        #     M = cv2.moments(largest_contour)
+        if contours:
+            # Assume the largest contour is the blue line
+            largest_contour = max(contours, key=cv2.contourArea)
+            M = cv2.moments(largest_contour)
             
-        #     if M["m00"] != 0:
-        #         # Calculate the center of the contour
-        #         cx = int(M["m10"] / M["m00"])
-        #         cy = int(M["m01"] / M["m00"])
+            if M["m00"] != 0:
+                # Calculate the center of the contour
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
+                print("LINE DETECTED!")  
                 
-        #         img_center = image.shape[1] // 2  # Get the center x-coordinate of the image
+                img_center = image.shape[1] // 2  # Get the center x-coordinate of the image
                 
-        #         if cx < img_center - 10:  # Threshold to avoid minor deviations
-        #             self.vehicle.pivot_left(0.5)
-        #         elif cx > img_center + 10:
-        #             self.vehicle.pivot_right(0.5)
-        #         else:
-        #             self.vehicle.drive_forward(0.8)
-        #     else:
-        #         print("Contour too small or not detected")  # Default action if contour is too small or not detected
-        #         self.vehicle.stop()
-        # else:
-        #     print("No blue line detected")  # Default action if no blue line is detected
-        #     self.vehicle.stop()
+                if cx < img_center - 40:  # Threshold to avoid minor deviations
+                    self.vehicle.pivot_right(0.35)
+                elif cx > img_center + 40:
+                    self.vehicle.pivot_left(0.35)
+                else:
+                    self.vehicle.drive_forward(0.5)
+            else:
+                print("Contour too small or not detected")  # Default action if contour is too small or not detected
+                self.vehicle.stop()
+        else:
+            print("No blue line detected")  # Default action if no blue line is detected
+            self.vehicle.stop()
 
         # if anything is detected by the sonic sensors, stop the car
         # stop = False
@@ -68,6 +69,3 @@ class Brain(base.Brain):
         #     if distance_sensor.distance < 0.25:
         #         self.vehicle.stop()
         #         stop = True
-
-        # if not stop:
-        #     self.vehicle.drive_forward()
